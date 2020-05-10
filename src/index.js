21bd48f869390f7b710e88808a6d992391d0c4d1
registoo <42060841+registoo@users.noreply.github.com>
2020-05-10 22:49:46 +0300

    first Ilya

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import CssBaseline from "@material-ui/core/CssBaseline";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <CssBaseline>
+      <App />
+    </CssBaseline>
   </React.StrictMode>,
   document.getElementById("root")
 );
