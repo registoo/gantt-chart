3261005a18651749e2e69f29bb1e616018cd08a2
AndreyGrek <an.gee@icloud.com>
2020-05-15 12:42:50 +0300

    Переделана структура папки Redux

--- src/redux/mainReducer/reducer.js
+++ src/redux/mainReducer/reducer.js
@@ -1,4 +1,4 @@
-import data from "../../../data";
+import data from "../../data";
 import defaultState from "./defaultState.js";
 
 export default function testReducer(state = defaultState(data), action) {
