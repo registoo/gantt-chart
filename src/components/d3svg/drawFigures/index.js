df21951ea478d2f98d843bc9417e4de7043b6847
registoo <42060841+registoo@users.noreply.github.com>
2020-05-17 18:14:31 +0300

    комментрай к рисовалющику фигур

--- src/components/d3svg/drawFigures/index.js
+++ src/components/d3svg/drawFigures/index.js
@@ -5,12 +5,14 @@ import * as d3 from "d3";
 export default function (state) {
   const svg = "svg#chart";
 
+  // обёртка для всех rect
   const gForRects = d3
     .select(svg)
     .append("g")
     .attr("id", "gForRects")
     .attr("transform", `translate(${state.sizesSVG.margin.left},${state.sizesSVG.margin.top})`);
 
+  // обёртка для всех brush
   const gForBrushing = d3.select(svg).append("g").attr("id", "gForBrushing");
 
   drawRect(gForRects, state);
