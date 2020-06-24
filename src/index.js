ca2c34ed479387ff3bbd43a36d1545862166e853
ILYA G <ilya21@pm.me>
2020-06-24 16:00:03 +0300

    пофиксли фигню

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import "./css/index.css";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+import * as serviceWorker from "./auxFunctions/serviceWorker";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { Provider } from "react-redux";
 import store from "./redux/store.js";
