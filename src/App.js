2c4da35813a143fda3a83c7aebb49576f8f22ef2
Гречишкин Андрей Викторович <agrechishkin@phoenixit.ru>
2020-07-25 01:52:35 +0300

    add top menu

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from "react";
+import { Container } from "@material-ui/core";
 import D3svg from "./components/d3svg";
 import WorksList from "./components/worksList";
-import { Container } from "@material-ui/core";
 import MainResizer from "./components/mainResizer/index.js";
 import LeftMenu from "././components/leftMenu";
 import Slider from "./components/d3svg/slider";
 import Accordion from "./components/accordion.js";
+import TopMenu from "./components/TopMenu";
 
 function App(props) {
   return (
-    <Container maxWidth="xl">
-      <LeftMenu />
+    <Container maxWidth="false" style={{ padding: "0px" }}>
+      <TopMenu />
+      {/* <LeftMenu /> */}
       <Slider />
       <MainResizer>
         <Accordion />
---
+++ src/components/TopMenu/index.js
@@ -0,0 +1,109 @@
+import React from "react";
+import { connect } from "react-redux";
+import Box from "@material-ui/core/Box";
+import logo from "../../assets/images/logo.svg";
+import "./styles.css";
+
+const Polzynok = (props) => {
+  return (
+    <Box className="top-menu__container" boxShadow={3}>
+      <img className="logo" src={logo} alt="" />
+      <div className="top-menu-buttons__container">
+        <div className="top-menu__button" tabindex="0">
+          Файл
+          <Box className="top-menu-context__container" boxShadow={3}>
+            <div className="top-menu-context">
+              <span className="top-menu-context__name">Lorem</span>
+              <span className="top-menu-context__shortcut">Alt+F4</span>
+            </div>
+            <div className="top-menu-context">
+              <span className="top-menu-context__name">Ipsum</span>
+              <span className="top-menu-context__shortcut">F4</span>
+            </div>
+            <hr />
+            <div className="top-menu-context">
+              <span className="top-menu-context__name">Отменить действие</span>
+              <span className="top-menu-context__shortcut">Ctrl+Z</span>
+            </div>
+          </Box>
+        </div>
+        <div className="top-menu__button" tabindex="1">
+          Редактировать
+          <Box className="top-menu-context__container" boxShadow={3}>
+            <div className="top-menu-context">
+              <span className="top-menu-context__name">Lorem</span>
+              <span className="top-menu-context__shortcut">Alt+F4</span>
+            </div>
+            <div className="top-menu-context">
+              <span className="top-menu-context__name">Ipsum</span>
+              <span className="top-menu-context__shortcut">F4</span>
+            </div>
+            <hr />
+            <div className="top-menu-context">
+              <span className="top-menu-context__name">Отменить действие</span>
+              <span className="top-menu-context__shortcut">Ctrl+Z</span>
+            </div>
+            <div className="top-menu-context">
+              <span className="top-menu-context__name">Lorem Ipsum LoremIpsum Lorem</span>
+              <span className="top-menu-context__shortcut">Alt+F4</span>
+            </div>
+            <div className="top-menu-context">
+              <span className="top-menu-context__name">Ipsum</span>
+              <span className="top-menu-context__shortcut">F4</span>
+            </div>
+            <hr />
+            <div className="top-menu-context">
+              <span className="top-menu-context__name">Отменить действие</span>
+              <span className="top-menu-context__shortcut">Ctrl+Z</span>
+            </div>
+          </Box>
+        </div>
+        <div className="top-menu__button" tabindex="2">
+          Помощь
+          <Box className="top-menu-context__container" boxShadow={3}>
+            <div className="top-menu-context">
+              <span className="top-menu-context__name">Lorem</span>
+              <span className="top-menu-context__shortcut">Alt+F4</span>
+            </div>
+            <div className="top-menu-context">
+              <span className="top-menu-context__name">Ipsum</span>
+              <span className="top-menu-context__shortcut">F4</span>
+            </div>
+            <hr />
+            <div className="top-menu-context">
+              <span className="top-menu-context__name">Отменить действие</span>
+              <span className="top-menu-context__shortcut">Ctrl+Z</span>
+            </div>
+            <div className="top-menu-context">
+              <span className="top-menu-context__name">Ipsum</span>
+              <span className="top-menu-context__shortcut">F4</span>
+            </div>
+            <div className="top-menu-context">
+              <span className="top-menu-context__name">Lorem</span>
+              <span className="top-menu-context__shortcut">Alt+F4</span>
+            </div>
+            <div className="top-menu-context">
+              <span className="top-menu-context__name">Ipsum</span>
+              <span className="top-menu-context__shortcut">F4</span>
+            </div>
+            <hr />
+            <div className="top-menu-context">
+              <span className="top-menu-context__name">Отменить действие</span>
+              <span className="top-menu-context__shortcut">Ctrl+Z</span>
+            </div>
+            <div className="top-menu-context">
+              <span className="top-menu-context__name">Ipsum</span>
+              <span className="top-menu-context__shortcut">F4</span>
+            </div>
+          </Box>
+        </div>
+      </div>
+    </Box>
+  );
+};
+
+const getState = (state) => {
+  return {};
+};
+
+export default connect(getState)(Polzynok);
