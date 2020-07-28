7bcdd3019b51a70ba4b95960637c9142ec863a19
ILYA G <ilya21@pm.me>
2020-07-28 23:56:52 +0600

    app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React from "react";
 import { Container } from "@material-ui/core";
 import D3svg from "./components/d3svg";
-import WorksList from "./components/worksList";
 import MainResizer from "./components/mainResizer/index.js";
-import LeftMenu from "././components/leftMenu";
+// import LeftMenu from "././components/leftMenu";
 import Slider from "./components/d3svg/slider";
 import Accordion from "./components/accordion.js";
-import TopMenu from "./components/TopMenu";
+// import TopMenu from "./components/TopMenu";
 
 function App(props) {
   return (
     <Container maxWidth={false} style={{ padding: "0px" }}>
-      <TopMenu />
-      <LeftMenu />
+      {/* <TopMenu />
+      <LeftMenu /> */}
       <Slider />
       <MainResizer>
         <Accordion />
