0858e00f3192c36bcc23843b96a7c6aaf2aeafde
ILYA G <ilya21@pm.me>
2020-09-12 16:40:32 +0600

    добавление детей по кнопке

--- src/components/planningWorks/addWork.js
+++ src/components/planningWorks/addWork.js
@@ -3,7 +3,12 @@ import * as d3 from "d3";
 
 export default (parent, addWorkLvl4, hierarchyFullData) => {
   const Node = d3.hierarchy.prototype.constructor;
-  const child = Object.assign(new Node({ data: parent.data.data }), {
+  const data = {};
+  data.jobName = { ...parent.data.data.jobName };
+  data.jobName.formattedText = "";
+  data.brushedData = [];
+  data.lvl4Dates = [];
+  const child = Object.assign(new Node({ data }), {
     parent,
     depth: parent.depth + 1,
   });
